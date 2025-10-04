6a43ecf7e249c21915a415641026844f5e774660
Makai Post <postmakai@gmail.com>
2025-10-04 14:12:14 -0700

    fix: correct import path for HomePage component and format tsconfig.json

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.sass'
-import HomePage from './pages/home'
+import HomePage from './pages/home/home.page'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HomePage />
-  </StrictMode>,
+  </StrictMode>
 )
