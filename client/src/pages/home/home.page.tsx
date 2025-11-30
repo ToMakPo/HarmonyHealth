be42b80108a7bbf186f97f684f99a8e4423dda2d
Makai Post <postmakai@gmail.com>
2025-11-29 18:29:21 -0800

    feat: enhance HomePage with hero section and additional content; add styles for responsive design

--- client/src/pages/home/home.page.tsx
+++ client/src/pages/home/home.page.tsx
@@ -4,6 +4,8 @@ import CustomerRegisterModal from "../header/customer-register.modal"
 import type { ModalRef } from "../../components/modal/modal.component"
 import { getImagePath } from "../../lib/utils"
 
+import "./home.styles.sass"
+
 const HomePage: React.FC = () => {
 	////////////////////
 	/// HERO SECTION ///
@@ -12,8 +14,19 @@ const HomePage: React.FC = () => {
 
 	const heroImageUrl = getImagePath('hero_image.png')
 	const heroSection = (
-		<section className="hero-section">
-			<img src={heroImageUrl} alt="Hero" className="hero-image" />
+		<section id="hero-section" 
+		style={{backgroundImage: `url(${heroImageUrl})`}}
+		>
+			<div id="hero-overlay">
+				<div id="hero-text">
+					<span id="hero-title">
+						<h2>Welcome to</h2>
+						<h1>Harmony Health</h1>
+					</span>
+					<p>Your partner in achieving optimal health and wellness.</p>
+					<button id="get-started-button">Get Started</button>
+				</div>
+			</div>
 		</section>
 	)
 
@@ -56,6 +69,10 @@ const HomePage: React.FC = () => {
 	return <>
 		<div id='home-page' className='page'>
 			{heroSection}
+
+			<h1>Welcome to Harmony Health</h1>
+			<p>Your partner in achieving optimal health and wellness. At Harmony Health, we believe that true well-being comes from a balance of mind, body, and spirit. Our dedicated team of healthcare professionals is committed to providing personalized care tailored to your unique needs.</p>
+			<p>Whether you're seeking preventive care, managing a chronic condition, or simply looking to enhance your overall health, Harmony Health is here to support you every step of the way. Join us on a journey towards a healthier, happier you.</p>
 		</div>
 
 		{loginModal}
